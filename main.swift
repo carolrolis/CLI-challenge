c3c16a14d1edf5469ed248abc4f8418930f0499d
Ana Carolina <126017853+carolrolis@users.noreply.github.com>
2024-10-23 21:57:07 -0300

    Update main.swift

--- main.swift
+++ main.swift
@@ -1,21 +1,22 @@
 import Foundation
 
-print("Digite um texto para análise:")
-let text = readLine() ?? ""
+print("Digite uma palavra para gerar anagramas:")
+let palavra = readLine()
 
-// Transforma o texto em um array de palavras atribuído à variável 'let'
-let words = text.split(separator: " ").map { String($0) }
+var letras = Array(palavra)
+var anagramas: Set<String> = []
 
-// Mostra no console o número de palavras
-print("Número de palavras: \(words.count)")
-
-// Busca uma palavra específica e diz quantas vezes ela aparece na frase
-print("Digite a palavra que deseja buscar:")
-let searchWord = readLine() ?? ""
-var searchCount = 0
-for word in words {
-    if word == searchWord {
-        searchCount += 1
+for i in 0..<letras.count {
+    for j in i+1..<letras.count {
+        var novaPalavra = letras
+        let letraAtual = novaPalavra[i]
+        novaPalavra[i] = novaPalavra[j]
+        novaPalavra[j] = letraAtual
+        anagramas.insert(String(novaPalavra))
     }
 }
-print("A palavra '\(searchWord)' aparece \(searchCount) vezes.")
+
+print("Anagramas gerados:")
+for anagrama in anagramas {
+    print(anagrama)
+}
